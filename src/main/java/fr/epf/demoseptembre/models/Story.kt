8a00bda2195cf4829bc47e0064e4101b7fdd32d6
ThibaultRoussin <thibault.roussin@epfedu.fr>
2018-10-04 00:12:41 +0200

    1er commit

---
+++ src/main/java/fr/epf/demoseptembre/models/Story.kt
@@ -0,0 +1,17 @@
+package fr.epf.demoseptembre.models
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+/**
+ * Cette classe utilise Kotlin (parce qu'on est grave des Hipsters).
+ * La "data class" représente un "POJO" (plain-old java object), c'est à dire un objet qui n'est destiné qu'à "représenter" une entité.
+ * Ici, en l'occurence, un utilisateur.
+ * @author Loïc Ortola on 10/09/2018
+ */
+@Entity
+data class Story(@Id @GeneratedValue var story_id: Int? =  null, var name: String? = null, var summary: String? = null, @ManyToOne var user: User?= null) {
+}
+
